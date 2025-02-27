ba2ff3dd3dbda4595133d80f20523096916a403f
Renato Barreto <renato.barreto@edu.unifil.br>
2025-02-26 21:57:22 -0300

    Corrigindo os metodos HTTP e a identação da classe "ClientHttpConfiguration"

--- src/cliente/ClientHttpConfiguration.java
+++ src/cliente/ClientHttpConfiguration.java
@@ -1,33 +1,46 @@
 package br.com.alura.cliente;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 
 public class ClientHttpConfiguration {
-    
-    public HttpResponse<String> dispararRequisicaoPost(String uri, Object object)
-            throws Exception {
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(uri))
-                .header("Content-Type", "application/json")
-                .method("POST", HttpRequest.BodyPublishers.ofString(new Gson().toJson(object)))
-                .build();
 
-        return client.send(request, HttpResponse.BodyHandlers.ofString());
+    private final HttpClient client;
+    private final Gson gson;
+
+    public ClientHttpConfiguration() {
+        this.client = HttpClient.newHttpClient();
+        this.gson = new Gson();
     }
 
-    public HttpResponse<String> dispararRequisicaoGet(String uri) throws Exception {
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
+    public HttpResponse<String> dispararRequisicaoPost(String uri, Object object) {
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(uri))
-                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(gson.toJson(object)))
                 .build();
-        return client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            return client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException("Erro ao enviar requisição POST para: " + uri, e);
+        }
+    }
+
+    public HttpResponse<String> dispararRequisicaoGet(String uri) {
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(uri))
+                .GET()
+                .build();
+
+            return client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException("Erro ao enviar requisição GET para: " + uri, e);
+        }
     }
 }
